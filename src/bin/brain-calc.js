929d7e65e774fdca13e9478d5a669e0c1e52a9dc
Nikolas <niksan540@gmail.com>
2019-07-02 01:57:02 +0300

    second game

---
+++ src/bin/brain-calc.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import getRandomFloat from '../brain-calc-index.js'
+console.log('\nWelcome to the Brain Games!');
+console.log('What is the result of the expression?');
+console.log('');
+getRandomFloat();
---
+++ src/brain-calc-index.js
@@ -0,0 +1,55 @@
+import readlineSync from 'readline-sync';
+
+const getRandomFloat = () => {
+
+  const name = readlineSync.question('May I have your name? ');
+  console.log ('Hello ' + name + '!');
+  console.log('');
+  let num3 = 1; //результат случайного вычисления случайных чисел
+  let n = 1; //раунды
+
+
+  while (n <= 3) {
+
+     const num = Math.floor(Math.random() * 30);
+     const num2 = Math.floor(Math.random() * 30);
+     const arr = [num + num2, num - num2, num * num2];
+     const rand = Math.floor(Math.random() * arr.length);
+
+const quest = () => {
+
+  if (rand === 0){
+    console.log('Question: ' + num + ' + ' + num2);
+    num3 = num + num2;
+  }
+  else if (rand === 1){
+    console.log('Question: ' + num + ' - ' + num2);
+    num3 = num - num2;
+  }
+  else if (rand === 2){
+    console.log('Question: ' + num + ' * ' + num2);
+    num3 = num * num2;
+  }
+};
+
+quest();
+
+  const answer = readlineSync.question('Your answer:');
+
+  if ( answer == num3 && n <= 3) {
+     console.log('Correct!');
+     n += 1;
+  }
+  else if (answer !== num3) {
+     n += 3;
+     console.log('\'' + answer + '\' is wrong answer ;(. Correct answer was \'' + num3 + '\' . Let\'s try again, ' + name + '!');
+  }
+};
+
+if (n = 3) {
+console.log('Congratulations, ' + name + '!');
+};
+};
+
+
+export default getRandomFloat;
