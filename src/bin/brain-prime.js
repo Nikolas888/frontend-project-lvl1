f06b8febccd52082ecc66f5962c032ea26742acf
Nikolas <niksan540@gmail.com>
2019-07-05 01:50:42 +0300

    july 5

---
+++ src/bin/brain-prime.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import getRandomFloat from '../brain-prime-index.js'
+console.log('\nWelcome to the Brain Games!');
+console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+console.log('');
+getRandomFloat();
